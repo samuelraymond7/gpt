e76370f7a735f4d2cef0001d2defc9f327d2e29a
Samuel Raymond <samuelraymond900@gmail.com>
2024-02-01 10:56:21 +0700

    add assets

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css'
 const App = () => {
   return (
     <div className='App'>
-        <div className='gradiend__bg'>
+        <div className='gradient__bg'>
           <Navbar/>
           <Header/>
         </div>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {RiMenu3Line,RiCloseLine} from 'react-icons/ri'
+import './navbar.css'
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src="{logo}" alt="logo" />
+        </div>
+      </div>
+    </div>
   )
 }
 
