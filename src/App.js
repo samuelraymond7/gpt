e657b450f3f9d27c3ad7cb9b530b473940e66cbc
Samuel Raymond <samuelraymond900@gmail.com>
2024-02-01 10:26:34 +0700

    initial formatting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import {Article, Brand, CTA, Feature, Navbar} from './components'
+import {Footer,Blog,Possibility,Features,WhatGPT3,Header} from './containers'
+import {Brand, CTA, Navbar} from './components'
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+        <div className='gradiend__bg'>
+          <Navbar/>
+          <Header/>
+        </div>
+        <Brand/>
+        <WhatGPT3/>
+        <Features/>
+        <Possibility/>
+        <CTA/>
+        <Blog/>
+        <Footer/>
     </div>
   )
 }
