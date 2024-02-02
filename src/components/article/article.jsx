198499e976fe489957f158a7865a1cf7b3ec4a06
Samuel Raymond <samuelraymond900@gmail.com>
2024-02-02 16:58:52 +0700

    Blog section and article done

--- src/components/article/article.jsx
+++ src/components/article/article.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import './Article.css';
-const Article = () => {
+const Article = ({imgURL,date,title}) => {
   return (
-    <div>article</div>
+    <div className="gpt3__blog-container__article">
+      <div className="gpt3__blog-container__article-image">
+        <img src={imgURL} alt="blog" />
+      </div>
+      <div className='gpt3__blog-container__article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
--- src/containers/blog/blog.jsx
+++ src/containers/blog/blog.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Article from '../../components/article/Article'
+import {blog01,blog02,blog03,blog04,blog05} from './imports'
+import './Blog.css'
 
 const Blog = () => {
   return (
-    <div>blog</div>
+    <div className="gpt3__blog section__padding" id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening. We are bloogging about it.</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container__groupA">
+          <Article imgURL={blog01} date="Sep 26, 2023" title="GPT-3 and OpenAI is the future. Let us explore how it is?"/>
+        </div>
+        <div className="gpt3__blog-container__groupB">
+          <Article imgURL={blog02} date="Sep 26, 2023" title="GPT-3 and OpenAI is the future. Let us explore how it is?"/>
+          <Article imgURL={blog03} date="Sep 26, 2023" title="GPT-3 and OpenAI is the future. Let us explore how it is?"/>
+          <Article imgURL={blog04} date="Sep 26, 2023" title="GPT-3 and OpenAI is the future. Let us explore how it is?"/>
+          <Article imgURL={blog05} date="Sep 26, 2023" title="GPT-3 and OpenAI is the future. Let us explore how it is?"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
